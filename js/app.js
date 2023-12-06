e70fc65d9d760403fc9ee24739535a120977abbd
Yersonam <yersonpyh.colab@gmail.com>
2023-12-06 07:51:41 -0500

    Portafolio Finalizado C1

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ const btn = document.querySelector('.nav__top__contenedor__menuCenter');
 btn.addEventListener('click', function(){
     menu.classList.toggle('active');
 })
+
+const pdf = document.querySelector('.pdf');
+
+pdf.addEventListener('click', function(){
+    window.open('./docs/c.v.pdf', '_blank');
+})
